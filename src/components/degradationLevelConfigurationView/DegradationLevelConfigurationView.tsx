dfbeb786da4e1e7f018235ca55cb2d5dead40d10
Steffen Knoblauch <knoblauch.steffen@web.de>
2021-05-20 09:01:55 +0200

    adds degradationLevelConfigurationViewPaper

--- src/components/degradationLevelConfigurationView/DegradationLevelConfigurationView.tsx
+++ src/components/degradationLevelConfigurationView/DegradationLevelConfigurationView.tsx
@@ -1,10 +1,24 @@
+import { Paper } from '@material-ui/core';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import './DegradationLevelConfigurationView.css';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    degradationLevelConfigurationViewPaper: {
+        height: "100%",
+    }
+  }),
+);
 
 const DegradationLevelConfigurationView = () => {
 
+    const classes = useStyles()
+
     return (
         <div id="degradation-level-configuration-view-container">
+            <Paper className={classes.degradationLevelConfigurationViewPaper}>
 
+            </Paper>
         </div>
     );
 }
